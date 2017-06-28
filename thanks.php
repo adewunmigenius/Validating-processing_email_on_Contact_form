dcafd1f5e7f02be2207c7620e4d09b7bbab3a845
OJO-Ahmed <adewunmigenius@gmail.com>
2017-06-28 12:49:45 +0100

    displayed message after submitting the form

---
+++ thanks.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Mail acknowledgement</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<h1>Thank You</h1>
+<p>Thank you for your comments. We'll be in touch if necessary.</p>
+</body>
+</html>
