78a9bdde208339ff72a96061293dac566ffd1c2d
OJO-Ahmed <adewunmigenius@gmail.com>
2017-06-28 12:47:06 +0100

    php code to process, validate the input details, generate the message inputed and autoreply a feedback message

--- process_mail.php
+++ process_mail.php
@@ -1,6 +1,7 @@
 <?php
+$mailSent = false;
 $suspect = false;
-$pattern = '/Content-type:|Bcc:|Cc:/i';
+$pattern = '/Content-Type:|Bcc:|Cc:/i';
 
 function isSuspect($value, $pattern, &$suspect){
 	if(is_array($value)) {
@@ -26,5 +27,35 @@ if (!$suspect):
 			$$key = $value;
 		}
 	}
+	if (!$missing && !empty($email)):
+		$validemail = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
+		if($validemail){
+			$header[] = "Reply-to: $validemail";
+		} else{
+			$error['email'] = true;
+		}
+	endif;
+	// if no errors, create header and message body
+	if(!$missing && !$error):
+		$header[] = implode("\r\n", $header);
+		// Initialize message
+		$message = '';
+		foreach ($expected as $field):
+			if (isset($$field) && !empty($$field)){
+			$val = $$field;
+			}else{
+				$val = 'Not selected';
+			}
+			// if an array, expand to a comma-separated string
+			if(is_array($val)){
+				$val = implode(', ', $val);
+			}
+			// Replace underscore in the field names with spaces
+			$field = str_replace('_', ' ', $field);
+			$message .= ucfirst ($field) .":$val\r\n\r\n";
+		endforeach;
+		$message = wordwrap($message, 70);
+		$mailSent = mail($to, $subject, $message, $header, $authorized);
+	endif;
 endif;
 ?>
