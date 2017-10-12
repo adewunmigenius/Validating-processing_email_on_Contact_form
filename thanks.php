467036a8e57ccbbe8316d37d68a2d97acd1b5a4a
OJO Ahmed Adewunmi <adewunmigenius@gmail.com>
2017-10-12 19:24:49 +0100

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-<h1>Thank You</h1>
+<h1>Thank You!</h1>
 <p>Thank you for your comments. We'll be in touch if necessary.</p>
 </body>
 </html>
